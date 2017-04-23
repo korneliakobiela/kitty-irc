d79dbee25050a57dc2377df8ccb8d3aad77e4868
unknown <adrian.wieprzkowicz@gmail.com>
2017-04-23 19:40:02 +0200

    Config env
    * add babel
    * add vueify
    * setup vue project

---
+++ src/main.js
@@ -0,0 +1,9 @@
+var Vue = require('vue');
+var App = require('./app.vue');
+
+new Vue({
+    el: '#kitty-irc-app',
+    render: function (createElement) {
+        return createElement(App);
+    }
+});
